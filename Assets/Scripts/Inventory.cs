5b9c501230cf86d59552a853cfe866c1f43fecaf
PrinceEGY <ahmed25004@hotmail.com>
2022-04-25 16:28:40 +0200

    Adding Stack System

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject attatchedInventory;
+    public List<GameObject> items;
+    public List<bool> isFull;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+    
+}
