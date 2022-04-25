b0a2593e332b045b14ad231a5063163cfcd502c0
Injy ElSherbini <injy.elsherbini@hotmail.com>
2022-04-25 21:38:57 +0200

    fixed a bug in camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private float currentPosX;
+    private float currentPosY;
     private Vector3 velocity = Vector3.zero;
     
     private void Update(){
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z),
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x,currentPosY, transform.position.z),
          ref velocity, speed);
          
     }
     public void moveToRoom(Transform _newRoom){
-        currentPosX= _newRoom.position.x;
+        currentPosY= _newRoom.position.y;
 
     }
 }
