83eb7766f05720ce4eaa1bf68e83dbc8bc750ad9
mercury-i <injy.elsherbini@hotmail.com>
2022-04-29 03:58:47 +0200

    leeerrrrpppp

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,33 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] Transform player;
+    public Vector3 mainLvlMinPosition;
+    public Vector3 entryLvlMinPosition;
+    public Vector3 mainLvlMaxPosition;
+    public Vector3 entryLvlMaxPosition;
+    public Vector3 minPosition;
+    public Vector3 maxPosition;
     private Vector3 velocity = Vector3.zero;
+    public float doorYaxis;
+    private void Update()
+    {
 
-    
-    private void Update(){
-        
-        transform.position = Vector3.SmoothDamp(
-            transform.position, 
-                new Vector3(player.position.x,
-                player.position.y, transform.position.z),
-            ref velocity, speed);
+        if (doorYaxis > player.transform.position.y)
+        {
+            moveToEntryLvl();
+            zoom(7, 10);
+        }
+        else if (doorYaxis < player.transform.position.y)
+        {
+            moveToMainLvl();
+            zoom(10, 7);
+        }
+
+        Vector3 targetPosition = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+        targetPosition.x = Mathf.Clamp(transform.position.x, minPosition.x, maxPosition.x);
+        targetPosition.y = Mathf.Clamp(transform.position.y, minPosition.y, maxPosition.y);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 8);
 
     }
 
-    public void moveToRoom(Transform _newRoom){
-       float currentPosY= _newRoom.position.y;
+
+    public void moveToMainLvl()
+    {
+        minPosition = mainLvlMinPosition;
+        maxPosition = mainLvlMaxPosition;
+
     }
 
-    public void zoom(float startSize, float targetSize){
-         for(float t = 0f; t < 100; t += Time.deltaTime) {
-        float blend = t/100;
-        Camera.main.orthographicSize = Mathf.SmoothStep(startSize, targetSize, blend); 
-         }
+    public void moveToEntryLvl()
+    {
+        minPosition = entryLvlMinPosition;
+        maxPosition = entryLvlMaxPosition;
+
     }
-    
+
+    public void zoom(float startSize, float targetSize)
+    {
+        for (float t = 0f; t < 100; t += Time.deltaTime)
+        {
+            float blend = t / 100;
+            Camera.main.orthographicSize = Mathf.SmoothStep(startSize, targetSize, blend);
+        }
+    }
+
 }
--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,28 +4,27 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-   [SerializeField] private Transform entryLevel;
-   [SerializeField] private Transform mainLevel;
+
    [SerializeField] private CameraController camera;
-   [SerializeField] private Transform Bug;
-    private float mainLvlSize=10;
-    private float entryLvlSize=7;
+    public float mainLvlSize=10;
+    public float entryLvlSize=7;
     private Vector3 bugVelocity= Vector3.zero;  
-   private void OnTriggerEnter2D(Collider2D collision){
+    
+    private void OnTriggerEnter2D(Collider2D collision){
        if (collision.tag == "Player")
        {
-           if (collision.transform.position.y>transform.position.y)
-           {
-               camera.moveToRoom(entryLevel);
-               camera.zoom(entryLvlSize,mainLvlSize);
+    //        if (collision.transform.position.y>transform.position.y)
+    //        {
+    //             camera.moveToEntryLvl();
+    //            camera.zoom(entryLvlSize,mainLvlSize);
 
-           }
-           else if(collision.transform.position.y<transform.position.y)
-           {
-               camera.moveToRoom(mainLevel);
-               camera.zoom(mainLvlSize,entryLvlSize);
+    //        }
+    //        else if(collision.transform.position.y<transform.position.y)
+    //        {camera.moveToMainLvl();
+              
+    //            camera.zoom(mainLvlSize,entryLvlSize);
 
-           }
+    //        }
        }
        
      }
