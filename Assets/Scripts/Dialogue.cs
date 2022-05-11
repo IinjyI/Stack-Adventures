6e58de96821bf47f0924ca5826d046b0ebb67bfd
PrinceEGY <ahmed25004@hotmail.com>
2022-05-11 04:13:57 +0200

    updating dialogues

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [System.Serializable]
 public class Dialogue{
-    public string name;
+    //public string name;
     [TextArea(2,10)]
     public string[] sentences;
 }
--- Assets/Scripts/DialogueTrigger.cs
+++ Assets/Scripts/DialogueTrigger.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
-
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
