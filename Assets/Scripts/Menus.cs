7daed2e0c7a8463afeb0a386bad5d7f05afd76f1
mercury-i <injy.elsherbini@hotmail.com>
2022-05-12 08:00:10 +0200

    pause menu #2

--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
 public class Menus : MonoBehaviour
 {
     public void NewGame()
@@ -13,4 +15,11 @@ public class Menus : MonoBehaviour
     {
         Application.Quit();
     }
+      public AudioMixer audioMixer;
+   public void setVolume(float volume){
+       audioMixer.SetFloat("volume", volume);
+   }
+   public void setFullScreen(bool isFullScreen){
+       Screen.fullScreen=isFullScreen;
+   }
 }
