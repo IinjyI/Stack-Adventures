4df54d219e6e28525ef4e20ab88b0992fc566e71
mercury-i <injy.elsherbini@hotmail.com>
2022-05-12 02:41:31 +0200

    fixed a bug

---
+++ Assets/Scripts/LevelSelection.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelSelection : MonoBehaviour
+{
+    public Button[] lvlButtons; 
+    void Start()
+    {
+        int levelAt = PlayerPrefs.GetInt("levelAt",1);
+        for(int i = 0; i< lvlButtons.Length; i++){
+            if(i+1 > levelAt){
+                lvlButtons[i].interactable=false;
+            }
+        }
+    }
+
+}
