80a660647942f4fb25a93ebafe0760e770d17bb0
mercury-i <injy.elsherbini@hotmail.com>
2022-05-12 03:57:05 +0200

    added exprimental scene

--- Assets/Scripts/CutSceneTrigger.cs
+++ Assets/Scripts/CutSceneTrigger.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 public class CutSceneTrigger : MonoBehaviour
@@ -14,15 +13,8 @@ public class CutSceneTrigger : MonoBehaviour
         cutScene.Play();
         mainCamera.GetComponent<CameraController>().enabled = false;
         player.GetComponent<PlayerMovment>().enabled = false;
-        OnLevelCompleted();
         StartCoroutine(Waiter());
     }
-    public void OnLevelCompleted()
-{
-    // Retrieve name of current scene / level
-    string sceneName = UnityEngine.SceneManagement.SceneManager.GetActiveScene().name;
-    PlayerPrefs.SetInt( sceneName, 1 ) ; // Indicates the level is completed
-}
     IEnumerator Waiter()
     {
         yield return new WaitForSeconds(5);
--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -4,22 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 public class LevelSelection : MonoBehaviour
 {
-    public string SceneName ; // Indicate which level this button must load once you click on it. Be carefull, the name must be the same as in your Build Settings
-
- protected void Awake()
- {
-     UnityEngine.UI.Button button = GetComponent<UnityEngine.UI.Button>();
-
-     if( button != null )
-     {
-          // Make the button load the given scene
-          button.onClick.AddListener( () => UnityEngine.SceneManagement.SceneManager.LoadScene( SceneName ) ) ;
-
-          // Make the button interactable only if the given scene / level has been completed
-          button.interactable = PlayerPrefs.GetInt( SceneName ) > 0 ;
-     }
-     else
-         Debug.LogWarning("No button component attached", this ) ;
- }
+    public Button[] lvlButtons; 
+    void Start()
+    {
+        int levelAt = PlayerPrefs.GetInt("levelAt",1);
+        for(int i = 0; i< lvlButtons.Length; i++){
+            if(i+1 > levelAt){
+                lvlButtons[i].interactable=false;
+            }
+        }
+    }
 
 }
