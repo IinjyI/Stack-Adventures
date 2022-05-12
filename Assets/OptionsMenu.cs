6b873e9e2244219e7cc67f1899edf9705b721aee
mercury-i <injy.elsherbini@hotmail.com>
2022-05-12 05:57:00 +0200

    volume #2 done

--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public AudioMixer audioMixer;
    public void setVolume(float volume){
-
+       audioMixer.SetFloat("volume", volume);
    }
 }
